3cfd4a52799ab84a8e33561567ad3a8b760212d2
Val <valentin.trouillet.ext@groupe-eduservices.fr>
2026-05-09 13:47:31 +0200

    Fix music migration: remove historicalitem, fix max_length

--- src/app/migrations/0061_add_music_media_type.py
+++ src/app/migrations/0061_add_music_media_type.py
@@ -1,13 +1,8 @@
-"""
-Migration: Add 'music' media type to Item.media_type choices.
-
-Run after: app.0060_fix_reopened_completed_tv_seasons
-"""
-
-from django.db import migrations, models
+﻿from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
+    """Migration to add music media type."""
 
     dependencies = [
         ("app", "0060_fix_reopened_completed_tv_seasons"),
@@ -19,39 +14,20 @@ class Migration(migrations.Migration):
             name="media_type",
             field=models.CharField(
                 choices=[
-                    ("movie", "Movie"),
-                    ("tv", "TV"),
-                    ("season", "Season"),
+                    ("tv", "TV Show"),
+                    ("season", "TV Season"),
                     ("episode", "Episode"),
+                    ("movie", "Movie"),
                     ("anime", "Anime"),
                     ("manga", "Manga"),
                     ("game", "Game"),
                     ("book", "Book"),
                     ("comic", "Comic"),
-                    ("boardgame", "Board Game"),
-                    ("music", "Music"),       # ← NEW
+                    ("boardgame", "Boardgame"),
+                    ("music", "Music"),
                 ],
-                max_length=20,
-            ),
-        ),
-        migrations.AlterField(
-            model_name="historicalitem",
-            name="media_type",
-            field=models.CharField(
-                choices=[
-                    ("movie", "Movie"),
-                    ("tv", "TV"),
-                    ("season", "Season"),
-                    ("episode", "Episode"),
-                    ("anime", "Anime"),
-                    ("manga", "Manga"),
-                    ("game", "Game"),
-                    ("book", "Book"),
-                    ("comic", "Comic"),
-                    ("boardgame", "Board Game"),
-                    ("music", "Music"),       # ← NEW
-                ],
-                max_length=20,
+                default="movie",
+                max_length=10,
             ),
         ),
     ]
